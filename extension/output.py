cc35981f75e6072f7e7c8150cdec1e6307a41b25
thaibault <t.sickert@gmail.com>
2013-05-10 20:08:29 +0200

    auto

--- extension/output.py
+++ extension/output.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 
+# vim: set tabstop=4 shiftwidth=4 expandtab:
+# vim: foldmethod=marker foldmarker=region,endregion:
+
 # region header
 
 ## python3.3 pass
