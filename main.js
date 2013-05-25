fbadbf4a624f8befabc285eeb8d91c30d959409d
thaibault <t.sickert@gmail.com>
2013-05-25 20:19:05 +0200

    Update readme file

---
+++ main.js
@@ -0,0 +1,2 @@
+/// region build/javaScript/jquery-1.9.1 build/javaScript/jquery-1.9.1.js build/javaScript/jquery-tools-1.0.js build/javaScript/bootstrap-2.3.1 build/javaScript/bootstrap-2.3.1.js build/javaScript/jquery-scrollTo-1.4.3.1 build/javaScript/jquery-scrollTo-1.4.3.1.js build/javaScript/jquery-touchwipe.1.1.1
+
