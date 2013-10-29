d037db6c7d3d0ec209333d4d9ebf8cb4434876cc
thaibault <t.sickert@gmail.com>
2013-10-29 23:14:39 +0100

    Automatic update to new version.

--- runnable/template.py
+++ runnable/template.py
@@ -452,13 +452,15 @@ class Parser(Class, Runnable):
 ##             'print': self._print, 'include': self._include,
 ##             'str': builtins.str, 'len': builtins.len, 'json': json,
 ##             'path_name_to_url': urllib.request.pathname2url, 'False': False,
-##             'True': True, 'locals': builtins.locals})
+##             'True': True, 'locals': builtins.locals,
+##             'sorted': builtins.sorted})
         self._builtins.update({
             '__indent__': self.indent, 'FileHandler': FileHandler,
             'print': self._print, 'include': self._include,
             'str': builtins.str, 'len': builtins.len, 'json': json,
             'path_name_to_url': urllib.pathname2url, 'False': False,
-            'True': True, 'locals': builtins.locals})
+            'True': True, 'locals': builtins.locals,
+            'sorted': builtins.sorted})
 ##
         return self._builtins
 
