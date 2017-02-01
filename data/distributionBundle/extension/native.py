a48fb0347d84f9468fc7364447cbc5ef9c0b2eb5
torben <t.sickert[~at~]gmail.com>
2017-02-01 08:17:01 +0100

    Update project homepage content.

--- data/distributionBundle/extension/native.py
+++ data/distributionBundle/extension/native.py
@@ -1713,9 +1713,13 @@ class Object(Class):
             class_name = String(
                 type_description
             ).delimited_to_camel_case.camel_case_capitalize.content
-            if description == type_description or description.endswith(
+            if description in [
+                class_name, type_description
+            ] or description.endswith(
                 '_%s' % type_description
-            ) or description.endswith(class_name):
+            ) or description.endswith(class_name) or description == '%s%s' % (
+                class_name[0].lower(), class_name[1:]
+            ):
                 return builtins.globals()[class_name](self.content).content
 
     @JointPoint
